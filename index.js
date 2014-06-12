aa2d13f90721994c115a7213e22cd0388ee566ab
Andrew Shaffer <andrew@weo.io>
2014-06-12 13:48:17 -0700

    fix default param bug; make logging configurable

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var jstat = require('jStat').jStat;
 
 module.exports = function(opts) {
   opts = opts || {};
-  opts.mean = opts.mean || 1000;
-  opts.variance = opts.variance || (opts.mean / 10);
+  if(opts.mean === undefined)
+    opts.mean = 1000;
+  if(opts.variance === undefined)
+    opts.variance = opts.mean / 10;
   return function(req, res, next) {
     var latency = jstat.normal.sample(opts.mean, opts.variance);
-    console.log(req.path, ': injecting latency: ', latency);
+    opts.log && console.log(req.path, ': injecting latency: ', latency);
     setTimeout(next, latency);
   };
 };
